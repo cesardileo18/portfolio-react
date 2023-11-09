12ab179392d27349a0dfed10fa3532fbb0966ee0
cesar.dileonardo <cesardileo18@gmail.com>
2023-11-09 13:20:27 -0300

    Se agrega spinner en el componente contact

---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,18 @@
+import { Audio, FidgetSpinner } from 'react-loader-spinner'
+const Spinner = () => {
+
+    return (
+        <FidgetSpinner
+            visible={true}
+            height="200"
+            width="200"
+            ariaLabel="dna-loading"
+            wrapperStyle={{}}
+            wrapperClass="dna-wrapper"
+            ballColors={['#F4442E', '#F4442E', '#F4442E']}
+            backgroundColor="#e0e0e0"
+        />
+    );
+}
+
+export default Spinner;
--- src/Views/Contacto.jsx
+++ src/Views/Contacto.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAt, faComment, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import Spinner from '../Components/Spinner';
 import '../Css/contacto.css';
 
 const Contacto = () => {
@@ -11,7 +12,11 @@ const Contacto = () => {
     const [subject, setSubject] = useState('');
     const [message, setMessage] = useState('');
     const [showAlert, setShowAlert] = useState(false);
+    const [showSpinner, setShowSpinner] = useState(false);
+
     const [alertMessage, setAlertMessage] = useState('');
+    const [responseMenssage, setResponseMenssage] = useState();
+
 
     const toggleFormulario = () => {
         setFormularioVisible(!formularioVisible);
@@ -19,10 +24,10 @@ const Contacto = () => {
     };
     const handleSubmit = async (e) => {
         e.preventDefault();
-        // Aquí debes enviar los datos al backend   https://backend-portfolio-c30x.onrender.com/
-    // https://backend-portfolio-c30x.onrender.com
         try {
-            const response = await fetch('https://backend-portfolio-c30x.onrender.com/api/enviar-correo', {
+            setShowSpinner(true)
+            // const response = await fetch('https://backend-portfolio-c30x.onrender.com/api/enviar-correo', {
+            const response = await fetch('http://localhost:8080/api/enviar-correo', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -30,6 +35,8 @@ const Contacto = () => {
                 body: JSON.stringify({ remitente: email, asunto: subject, mensaje: message }),
             });
             if (response.ok) {
+                setResponseMenssage(response)
+                setShowSpinner(false)
                 // El estado 200 indica que la respuesta es exitosa
                 setEmail('');
                 setSubject('');
@@ -40,78 +47,96 @@ const Contacto = () => {
                     setShowAlert(false);
                 }, 3000); // 3000 milisegundos (3 segundos)
             } else {
-                // La respuesta indica un error (por ejemplo, un estado 404, 500, etc.)
-                throw new Error('Hubo un error al enviar el correo');
+                setShowSpinner(false)
+                setEmail('');
+                setSubject('');
+                setMessage('');
+                setAlertMessage('Hubo un error al enviar el correo');
+                setShowAlert(true);
             }
         } catch (error) {
-            console.error('error', error);
+            setShowSpinner(false)
+            setEmail('');
+            setSubject('');
+            setMessage('');
             setAlertMessage('Hubo un error al enviar el correo');
             setShowAlert(true);
         }
     };
+    // {showSpinner && <Spinner />}
     const buttonClass = `pressed ${pressed ? 'button' : 'initialBtn'}`;
     return (
-        <div className='contain-form p-3'>
-            <h1 className='text-center'>Página de Contacto</h1>
-            <div className="cotainButton mt-5">
-                <div className='contain__iconContact'>
-                    <a href="https://github.com/cesardileo18" target="_blank" rel="noopener noreferrer">
-                        <button className="btn__Contact">
-                            <FontAwesomeIcon icon={faGithub} />
-                        </button>
-                    </a>
-                </div>
-                <div className='contain__iconContact'>
-                    <a href="https://www.linkedin.com/in/cesar-orlando-acacio-di-leonardo-12b01a1b5" target="_blank" rel="noopener noreferrer">
-                        <button className="btn__Contact">
-                            <FontAwesomeIcon icon={faLinkedinIn} />
-                        </button>
-                    </a>
-                </div>
-                <div className='contain__iconContact'>
-                    <button className={buttonClass} onClick={toggleFormulario}>
-                        <FontAwesomeIcon icon={faEnvelope} />
-                    </button>
-                </div>
-            </div>
-            {showAlert && (
-                <div className="alert">
-                    <p>{alertMessage}</p>
-                    <button onClick={() => setShowAlert(false)} className="close-button">
-                        X
-                    </button>
-                </div>
-            )}
-            {formularioVisible ? (
-                <div className='form'>
-                    <h2 className='mt-3 text-center'>Enviar Correo Electrónico</h2>
-                    <form className='form-control mt-3' onSubmit={handleSubmit}>
-                        <label className='form-label'>Correo Electrónico:</label>
-                        <div className='input-group'>
-                            <span className="input-group-text" id="basic-addon1">
-                                <FontAwesomeIcon icon={faAt} />
-                            </span>
-                            <input type="email" className='form-control' name={email} value={email} onChange={(e) => setEmail(e.target.value)} aria-describedby="basic-addon1" required />
-                        </div>
-                        <label className='form-label'>Asunto:</label>
-                        <div className='input-group'>
-                            <span className="input-group-text" id="basic-addon2">
-                                <FontAwesomeIcon icon={faComment} />
-                            </span>
-                            <input type="text" className='form-control' name={subject} value={subject} onChange={(e) => setSubject(e.target.value)} aria-describedby="basic-addon2" required />
-                        </div>
-                        <label className='form-label'>Mensaje:</label>
-                        <textarea value={message} className='form-control' name={message} onChange={(e) => setMessage(e.target.value)} required />
-                        <button type="submit">Enviar Correo</button>
-                    </form>
-                </div>
+        <div className='contain-contact-spinner'>
+            {showSpinner ? (
+                <Spinner />
             ) : (
-                <div className='agradecimiento mt-5 p-3'>
-                    {/* <p>¡Gracias por visitar mi página! Si deseas contactarme, puedes encontrarme en LinkedIn o hacerlo a través del formulario de correo electrónico. También puedes explorar mis proyectos y metodología de trabajo en mi <a href="https://github.com/tu-usuario-github">repositorio de GitHub</a>.</p> */}
-                    <h4>Gracias por visitar mi página! Si deseas contactarme, puedes encontrarme en LinkedIn o hacerlo a través del formulario de correo electrónico. También puedes explorar mis proyectos y metodología de trabajo en mi repositorio de GitHub.</h4>
+                <div className='contain-form p-3'>
+                    <h1 className='text-center'>Página de Contacto</h1>
+                    <div className="cotainButton mt-5">
+                        <div className='contain__iconContact'>
+                            <a href="https://github.com/cesardileo18" target="_blank" rel="noopener noreferrer">
+                                <button className="btn__Contact">
+                                    <FontAwesomeIcon icon={faGithub} />
+                                </button>
+                            </a>
+                        </div>
+                        <div className='contain__iconContact'>
+                            <a href="https://www.linkedin.com/in/cesar-orlando-acacio-di-leonardo-12b01a1b5" target="_blank" rel="noopener noreferrer">
+                                <button className="btn__Contact">
+                                    <FontAwesomeIcon icon={faLinkedinIn} />
+                                </button>
+                            </a>
+                        </div>
+                        <div className='contain__iconContact'>
+                            <button className={buttonClass} onClick={toggleFormulario}>
+                                <FontAwesomeIcon icon={faEnvelope} />
+                            </button>
+                        </div>
+                    </div>
+                    {showAlert && (
+                        <div className={`${responseMenssage?.status === 200 ? "alert" : "error-alert"}`}>
+                            <p>{alertMessage}</p>
+                            <button onClick={() => setShowAlert(false)} className="close-button mx-2">
+                                X
+                            </button>
+                        </div>
+                    )}
+                    {formularioVisible ? (
+                        <div className='form mt-4'>
+                            <h2 className='mt-3 text-center'>Enviar Correo Electrónico</h2>
+                            <form className='form-control mt-3' onSubmit={handleSubmit}>
+                                <label className='form-label'>Correo Electrónico:</label>
+                                <div className='input-group'>
+                                    <span className="input-group-text" id="basic-addon1">
+                                        <FontAwesomeIcon icon={faAt} />
+                                    </span>
+                                    <input type="email" className='form-control' name={email} value={email} onChange={(e) => setEmail(e.target.value)} aria-describedby="basic-addon1" required />
+                                </div>
+                                <label className='form-label'>Asunto:</label>
+                                <div className='input-group'>
+                                    <span className="input-group-text" id="basic-addon2">
+                                        <FontAwesomeIcon icon={faComment} />
+                                    </span>
+                                    <input type="text" className='form-control' name={subject} value={subject} onChange={(e) => setSubject(e.target.value)} aria-describedby="basic-addon2" required />
+                                </div>
+                                <label className='form-label'>Mensaje:</label>
+                                <textarea value={message} className='form-control' name={message} onChange={(e) => setMessage(e.target.value)} required />
+                                <div className='containBtn-submitEmail d-flex justify-content-center'>
+                                    <button type="submit" className='btn btn-secondary my-3'>Enviar Correo</button>
+                                </div>
+                            </form>
+                        </div>
+                    ) : (
+                        <div className='agradecimiento mt-5 p-3'>
+                            {/* <p>¡Gracias por visitar mi página! Si deseas contactarme, puedes encontrarme en LinkedIn o hacerlo a través del formulario de correo electrónico. También puedes explorar mis proyectos y metodología de trabajo en mi <a href="https://github.com/tu-usuario-github">repositorio de GitHub</a>.</p> */}
+                            <h4>Gracias por visitar mi página! Si deseas contactarme, puedes encontrarme en LinkedIn o hacerlo a través del formulario de correo electrónico. También puedes explorar mis proyectos y metodología de trabajo en mi repositorio de GitHub.</h4>
+                        </div>
+                    )}
                 </div>
             )}
-        </div>);
+        </div>
+
+    );
 }
 
 export default Contacto;
