71364191f07e9e85c50e1a50c77069178fe00a09
cesar.dileonardo <cesardileo18@gmail.com>
2023-11-09 08:43:49 -0300

    se cambia la configuracion de BrowserRouter a HashRouter

--- main.jsx
+++ main.jsx
@@ -2,10 +2,10 @@ import { createRoot } from "react-dom/client"
 import App from "./src/App"
 import "./style.css"
 import "bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 const root = createRoot(document.getElementById('app'))
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 )
