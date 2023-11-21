446b012bdb5cf88f30c7d7354c137b3c4224a851
cesar.dileonardo <cesardileo18@gmail.com>
2023-11-21 08:48:28 -0300

    Se cambian estilos para ajustar distintas pantallas a mobile, se agrega logica para el footer

--- src/Views/Footer.jsx
+++ src/Views/Footer.jsx
@@ -1,54 +1,84 @@
-import "../Css/footer.css"
+import { useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouseChimney, faLaptopCode, faBriefcase, faEnvelope, faLaptop, faCode } from '@fortawesome/free-solid-svg-icons';
-import { Link } from "react-router-dom";
+import { faHouseChimney, faLaptopCode, faBriefcase, faEnvelope, faCode } from '@fortawesome/free-solid-svg-icons';
+import { Link, useLocation } from "react-router-dom";
+import "../Css/footer.css";
+
 const Footer = () => {
+    const location = useLocation();
+    const [selectedIcon, setSelectedIcon] = useState(null);
+
+    const handleIconClick = (icon) => {
+        setSelectedIcon(icon);
+    };
+
+    const renderAdditionalDiv = () => {
+        if (selectedIcon) {
+            return (
+                <div className="additional-div">
+                </div>
+            );
+        }
+        return null;
+    };
 
     return (
         <footer>
             <section className="footer">
                 <Link to="/inicio">
-                    <div className="footer__icons" style={{ cursor: "pointer" }}>
+                    <div
+                        className={`footer__icons ${location.pathname === '/inicio' ? 'selected' : ''}`}
+                        style={{ cursor: "pointer" }}
+                        onClick={() => handleIconClick('inicio')}
+                    >
                         <FontAwesomeIcon icon={faHouseChimney} className="footer__icon" />
-                        <span className="footer__p">
-                            Inicio
-                        </span>
+                        <span className="footer__p">Inicio</span>
                     </div>
                 </Link>
                 <Link to="/projects">
-                    <div className="footer__icons" style={{ cursor: "pointer" }}>
+                    <div
+                        className={`footer__icons ${location.pathname === '/projects' ? 'selected' : ''}`}
+                        style={{ cursor: "pointer" }}
+                        onClick={() => handleIconClick('projects')}
+                    >
                         <FontAwesomeIcon icon={faCode} className="footer__icon" />
-                        <span className="footer__p">
-                        Proyectos
-                        </span>
+                        <span className="footer__p">Proyectos</span>
                     </div>
                 </Link>
                 <Link to="/expirence">
-                    <div className="footer__icons" style={{ cursor: "pointer" }}>
+                    <div
+                        className={`footer__icons ${location.pathname === '/expirence' ? 'selected' : ''}`}
+                        style={{ cursor: "pointer" }}
+                        onClick={() => handleIconClick('expirence')}
+                    >
                         <FontAwesomeIcon icon={faBriefcase} className="footer__icon" />
-                        <span className="footer__p">
-                        Experiencia
-                        </span>
+                        <span className="footer__p">Experiencia</span>
                     </div>
                 </Link>
                 <Link to="/skills">
-                    <div className="footer__icons" style={{ cursor: "pointer" }}>
+                    <div
+                        className={`footer__icons ${location.pathname === '/skills' ? 'selected' : ''}`}
+                        style={{ cursor: "pointer" }}
+                        onClick={() => handleIconClick('skills')}
+                    >
                         <FontAwesomeIcon icon={faLaptopCode} className="footer__icon" />
-                        <span className="footer__p">
-                        Habilidades
-                        </span>
+                        <span className="footer__p">Habilidades</span>
                     </div>
                 </Link>
                 <Link to="/contact">
-                    <div className="footer__icons" style={{ cursor: "pointer" }}>
+                    <div
+                        className={`footer__icons ${location.pathname === '/contact' ? 'selected' : ''}`}
+                        style={{ cursor: "pointer" }}
+                        onClick={() => handleIconClick('contact')}
+                    >
                         <FontAwesomeIcon icon={faEnvelope} className="footer__icon" />
-                        <span className="footer__p">
-                        Contacto
-                        </span>
+                        <span className="footer__p">Contacto</span>
                     </div>
                 </Link>
             </section>
+            {renderAdditionalDiv()}
         </footer>
     );
-}
+};
+
 export default Footer;
