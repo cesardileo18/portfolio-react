5ba4e8d902d8e8d9bcecea529d9164f7d8c1d03e
cesar.dileonardo <cesardileo18@gmail.com>
2023-11-21 09:20:28 -0300

    Se cambia la sintaxis de clases a metodologias bem en la patalla inicio

--- src/Views/Inicio.jsx
+++ src/Views/Inicio.jsx
@@ -1,4 +1,4 @@
-import "../Css/inicio.css"
+import "../Css/inicio.css";
 import ParticlesBackground from "../Components/ParticlesBackground";
 import { useState, useEffect } from 'react';
 import Spinner from '../Components/Spinner';
@@ -6,12 +6,14 @@ import ScrollButton from '../Components/ScrollButton';
 
 const Inicio = () => {
     const [showSpinner, setShowSpinner] = useState(false);
+
     useEffect(() => {
-        setShowSpinner(true)
+        setShowSpinner(true);
         setTimeout(() => {
-            setShowSpinner(false)
+            setShowSpinner(false);
         }, 1000);
-    }, [])
+    }, []);
+
     return (
         <div className="inicioPage">
             {showSpinner ? (
@@ -21,19 +23,19 @@ const Inicio = () => {
             ) : (
                 <div className="inicio-content">
                     <ParticlesBackground />
-                    <header className="inicio p-3">
-                        <h1 className="inicio__header__h1">Cesar Acacio Di Leonardo</h1>
-                        <img src="./img/imgCv.webp" alt="imagen Curriculum" className="inicio__header__img__cv ms-3" />
+                    <header className="inicio-header p-3">
+                        <h1 className="inicio-header__h1">Cesar Acacio Di Leonardo</h1>
+                        <img src="./img/imgCv.webp" alt="imagen Curriculum" className="inicio-header__img-cv__mobile ms-3" />
                     </header>
-                    <section className="inicio p-3" >
-                        <div className="inicio__img">
-                            <img src="./img/imgCv.webp" alt="imagen Curriculum" className="inicio__img__cv" />
+                    <section className="inicio-section p-3" >
+                        <div className="inicio-img-desktop">
+                            <img src="./img/imgCv.webp" alt="imagen Curriculum" className="inicio-img__cv__desktop" />
                         </div>
                         <h2 className="text-center mt-5">Programador Full Stack</h2>
-                        <div className="inicio__img__programacion mt-5">
-                            <img src="./img/imagInicio.png" alt="imagen Progamación" className="inicio__img__dev" />
+                        <div className="inicio-img-programacion mt-5">
+                            <img src="./img/imagInicio.png" alt="imagen Progamación" className="inicio-img__dev" />
                         </div>
-                        <article className="p-3 mt-5">
+                        <article className="inicio-article p-3 mt-5">
                             <h2>Sobre Mí</h2>
                             <p>Soy un desarrollador web con experiencia en el desarrollo full stack.
                                 Mi objetivo es seguir creciendo como profesional en el campo de desarrollo
@@ -45,7 +47,6 @@ const Inicio = () => {
             )}
             <ScrollButton />
         </div>
-
     );
 }
 
